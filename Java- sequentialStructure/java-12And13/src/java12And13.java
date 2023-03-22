618070be2c0bcc1f1b35b3088c8fd226ce08ed82
Wendrick Cardoso <wendrick.estudos@gmail.com>
2023-03-22 00:06:29 -0300

    Terminando a 12 e 13 questão

---
+++ Java- sequentialStructure/java-12And13/src/java12And13.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class java12And13 {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        String sexo = "";
+        
+        // .equals() compara o conteudo
+        while (!sexo.equals("M") && !sexo.equals("H")) {
+            System.out.println("Por favor, informe seu sexo (M - mulher/H - homem):");
+            sexo = scan.next().toUpperCase();
+        }   
+
+        System.out.print("Informe sua alture em metros:  ");
+        double altura = scan.nextDouble();
+        scan.close();
+
+        String output = String.format("Seu pedo ideal é %.2f kg!", 
+            (sexo.equals("M") ? ((altura*62.1)-44.7) : ((altura*72.7)-58)));
+
+        System.out.println(output);
+    }
+}
