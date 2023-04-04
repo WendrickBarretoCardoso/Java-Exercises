24a1bbebbd2009791bb8570bee6acb589b2b47d7
Wendrick Cardoso <wendrick.estudos@gmail.com>
2023-04-03 23:01:25 -0300

    Terminando a 2º lista VAMOS QUE VAMOS

--- Java-decisionStructure/java-26/src/java26.java
+++ Java-decisionStructure/java-26/src/java26.java
@@ -2,6 +2,40 @@ import java.util.Scanner;
 
 public class java26 {
     public static void main(String[] args) throws Exception {
+        posto();
+    }
+
+    public static void posto() {
         Scanner scan = new Scanner(System.in);
+        String output = "";
+        System.out.print("1 - Gasolina  |   2 - Álcool\nInforme qual o combustível que você gostaria? R: ");
+        int comb = scan.nextInt();
+        while (comb != 1 && comb != 2) {
+            System.out.print("Escolha incorreta, insira novamente: ");
+            comb = scan.nextInt();
+        }
+        System.out.print("Informe a quantidade de combustível que você deseja: ");
+        double quant = scan.nextDouble();
+        output = String.format("O valor do reabastecimento foi de: R$%.2f ",descount(comb, quant));
+        System.out.println(output);
+        scan.close();
+    }
+
+    public static double descount(int comb, double quant) {
+        double value = 0;
+        if (comb == 1) {
+            if (quant <= 20.0) {
+                value = (2.50*quant) - ((2.50*quant)*0.03);
+            } else {
+                value = (2.50*quant) - ((2.50*quant)*0.05);
+            }
+        } else {
+            if (quant <= 20.0) {
+                value = (1.9*quant) - ((1.9*quant)*0.04);
+            } else {
+                value = (1.9*quant) - ((1.9*quant)*0.06);
+            }
+        }
+        return value;
     }
 }
--- Java-decisionStructure/java-27/src/java27.java
+++ Java-decisionStructure/java-27/src/java27.java
@@ -2,6 +2,63 @@ import java.util.Scanner;
 
 public class java27 {
     public static void main(String[] args) throws Exception {
+        feira();
+    }
+
+    public static void feira() {
         Scanner scan = new Scanner(System.in);
+        boolean strawberry = getAnswers("Você gostaria de comprar morangos?\nO preço está R$2,50 o KG e se formais de 5kg fica R$2,20.\nInforme com S para SIM e N para Não!\nR: ", scan);
+        double stbQntd = Qntd(strawberry, scan);
+
+        boolean apples = getAnswers("\nVocê gostaria de comprar maçãs?\nO preço está R$1,80o KG e se formais de 5kg fica R$1,50.\nInforme com S para SIM e N para Não!\nR: ", scan);
+        double appQntd = Qntd(apples, scan);
+
+        price(stbQntd,appQntd);
+        scan.close();
+    }
+
+    public static boolean getAnswers(String frase, Scanner scan) {
+        System.out.print(frase);
+        String answ = scan.nextLine();
+        answ = answ.toUpperCase();
+        while (!answ.equals("S") && !answ.equals("N")) {
+            System.out.print("\nInfromção inválida repita a resposta:  ");
+            answ = scan.nextLine();
+            answ = answ.toUpperCase();
+        }
+        if (answ.equals("S")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static double Qntd(boolean conf, Scanner scan) {
+        double kg = 0;
+        if (conf) {
+            System.out.print("Quantos kg você gostaria? R: ");
+            kg = scan.nextDouble();
+        }
+        return kg;
+    }
+
+    public static void price(double stbQntd, double appQntd) {
+        String output = "";
+        double price = 0;
+        if (stbQntd <= 5) {
+            price += (stbQntd*2.50);
+        } else {
+            price += (stbQntd*2.20);
+        }
+        if (appQntd <= 5) {
+            price += (stbQntd*1.80);
+        } else {
+            price += (stbQntd*1.50);
+        }
+        if ((stbQntd+appQntd)>=8 || price >= 25.00) {
+            price = price - (price*0.1);
+        }
+        output = String.format("\nO peso foi de %.1fKg e o preço total ficou de R$%.2f.",(stbQntd+appQntd),price);
+        System.out.println(output);
     }
 }
