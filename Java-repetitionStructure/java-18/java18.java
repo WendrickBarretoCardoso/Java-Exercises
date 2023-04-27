a4931914c12dd1cdfb870ced33b0821440532a35
Wendrick Cardoso <wendrick.estudos@gmail.com>
2023-04-26 22:46:30 -0300

    Correção de algumas coisas

--- Java-repetitionStructure/java-18/java18.java
+++ Java-repetitionStructure/java-18/java18.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class java18 {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
+        scan.close();
     }
 }
